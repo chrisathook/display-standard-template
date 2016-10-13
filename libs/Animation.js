29a5d46953962a26eae557f6e61afcd244ea775a
Christopher Watts <chris@byhook.com>
2016-10-13 14:29:52 -0700

    moving handlers to correct spot

--- libs/Animation.js
+++ libs/Animation.js
@@ -2,16 +2,17 @@
 (function () {
   var Animation = function () {
 
-    var onComplete = function () {
-      console.log('animation complete');
-      document.getElementById('cta').addEventListener('click', clickHandler);
-      document.getElementById('catchAll').addEventListener('click', clickHandler)
-    };
-    var onStart = function () {
-    };
+
 
     // do all your animation in this function. Including any calls to get DOM elements.
     var render = function () {
+      var onComplete = function () {
+        console.log('animation complete');
+        document.getElementById('cta').addEventListener('click', clickHandler);
+        document.getElementById('catchAll').addEventListener('click', clickHandler)
+      };
+      var onStart = function () {
+      };
       var tl = null;
       // make additional timeline here.
       tl = new TimelineMax({
@@ -19,10 +20,9 @@
         onStart: onStart
       });
       tl.add(TweenMax.to('#adRoot', 1, {opacity: 1}));
-
       //customize this function so that when called it kills all animation timelines, etc.
-      return function destroy () {
-        console.log ('Kill Animations' );
+      return function destroy() {
+        console.log('Kill Animations');
         tl.kill();
       }
     };
