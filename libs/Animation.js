78eb4b8cb36946a48dd0ce1257b571f9b4e9fb54
Craig Albert <craig@byhook.com>
2016-12-20 13:54:46 -0500

    fixed scope issue with hover over/out handlers. made hover effect more visible.

--- libs/Animation.js
+++ libs/Animation.js
@@ -4,23 +4,26 @@
     // do all your animation in this function. Including any calls to get DOM elements.
     var render = function () {
       var ctaButton = document.getElementById('cta');
+      var overHandler = function () {
+        ctaButton.classList.add('cta-hover');
+      };
+
+      var outHandler = function () {
+        ctaButton.classList.remove('cta-hover');
+      };
+
       var onComplete = function () {
         console.log('animation complete');
-        var overHandler = function () {
-          ctaButton.classList.add('cta-hover')
-        };
-        var outHandler = function () {
-          ctaButton.classList.remove('cta-hover')
-        };
-        
         ctaButton.addEventListener('mouseover', overHandler);
         ctaButton.addEventListener('mouseout', outHandler);
       };
+
       var onStart = function () {
         document.getElementById('catchAll').addEventListener('click', clickHandler);
         ctaButton.addEventListener('click', clickHandler);
         ctaButton.addEventListener('click', outHandler);
       };
+      
       var tl = null;
       // make additional timeline here.
       tl = new TimelineLite({
