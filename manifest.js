027a07f6fa3c12a7ae794df00fecaab2ef527f55
Christopher Watts <chris@byhook.com>
2017-05-04 11:33:06 -0700

    working on set up

---
+++ manifest.js
@@ -0,0 +1,21 @@
+
+{
+    "version": "1.0",
+    "title": "300x250 Example Banner",
+    "description": "optional string", 
+    "width" : "300",
+    "height": "250",
+    "events": {
+        "enabled": 0,
+        "list": {
+            "1": "myEventName1",
+            "2": "myEventName2",
+            "3": "myEventName3"
+        }
+    },
+ 
+    "clicktags": {
+        "clickTAG": "https://www.test.com"
+    },
+    "source": "index.html"
+}
