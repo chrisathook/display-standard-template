4c8868669003fe90c4c83446397db644c55c6fe8
christopher watts <chris@byhook.com>
2018-03-02 11:25:52 -0800

    adding support for loading svgs from /img for dynamic system

--- js/Ad.js
+++ js/Ad.js
@@ -1,68 +1,83 @@
 'use strict';
-(function () {
-  var Ad = function () {
-    try {
-      console.log('Hello World ' + bowser.name + ' ' + bowser.version);
-    } catch (err) {
-    }
-    var api = {};
-    var rootSource = null;
-    var currentAnimationKill = null;
-    var firstRun = false;
-    var buildRoot = function () {
-      var newRoot = rootSource.cloneNode(true);
-      var oldRoot = document.getElementById("adRoot");
-      document.body.replaceChild(newRoot, oldRoot)
-    };
-    var show = function () {
-      var toShow = document.querySelectorAll('.invisible');
-      for (var i = 0; i < toShow.length; i++) {
-        toShow[i].classList.remove('invisible');
-      }
-      var unhide = document.querySelectorAll('.hidden');
-      for (var j = 0; j < unhide.length; j++) {
-        unhide[j].classList.remove('hidden');
-      }
-    };
-    var writeHTML = function (div, text) {
-      div.innerHTML = text;
-      console.log('loading done', div.id);
-    };
-    var processSVG = function (svgObj) {
-      for (var prop in svgObj) {
+(function() {
+    var Ad = function() {
         try {
-          writeHTML(document.getElementById(prop), svgObj[prop])
-        } catch (err) {
-          console.error('SVG Loading Failed', prop + ".svg");
-          console.error(err);
-        }
-      }
+            console.log('Hello World ' + bowser.name + ' ' + bowser.version);
+        } catch (err) {}
+        var api = {};
+        var rootSource = null;
+        var currentAnimationKill = null;
+        var firstRun = false;
+        var buildRoot = function() {
+            var newRoot = rootSource.cloneNode(true);
+            var oldRoot = document.getElementById("adRoot");
+            document.body.replaceChild(newRoot, oldRoot)
+        };
+        var show = function() {
+            var toShow = document.querySelectorAll('.invisible');
+            for (var i = 0; i < toShow.length; i++) {
+                toShow[i].classList.remove('invisible');
+            }
+            var unhide = document.querySelectorAll('.hidden');
+            for (var j = 0; j < unhide.length; j++) {
+                unhide[j].classList.remove('hidden');
+            }
+        };
+        var writeHTML = function(div, text) {
+            div.innerHTML = text;
+            console.log('loading done', div.id);
+        };
+        var processSVG = function(svgObj) {
+            for (var prop in svgObj) {
+                try {
+                    writeHTML(document.getElementById(prop), svgObj[prop])
+                } catch (err) {
+                  // handle dynamic SVGs
+                    var success = false;
+                    var divs = document.getElementsByTagName("div");
+                    for (var i = 0; i < divs.length; i++) {
+                        var theDiv = divs[i];
+                        if (theDiv.hasAttribute('inject')) {
+                            var theAttr = theDiv.getAttribute('inject');
+                            if (theAttr.search(prop) !== -1) {
+                                writeHTML(theDiv, svgObj[prop]);
+                                success = true;
+                            }
+                        }
+                    }
+                    if (success) {
+                        console.log('DYNAMIC SVG INLINED', prop + ".svg");
+                    } else {
+                        console.error('SVG Loading Failed', prop + ".svg");
+                        console.error(err);
+                    }
+                }
+            }
+        };
+        var setUpAd = function() {
+            var callback = function() {
+                processSVG(window.bannerSvgData);
+                show();
+                rootSource = document.getElementById("adRoot").cloneNode(true);
+                currentAnimationKill = Animation();
+            };
+            var loader = window.svgImageLoader;
+            loader(window.bannerSvgData, callback);
+        };
+        api.init = function() {
+            if (firstRun) {
+                return
+            }
+            firstRun = true;
+            console.log('ad int');
+            setUpAd();
+        };
+        api.replay = function() {
+            currentAnimationKill();
+            buildRoot();
+            Animation();
+        };
+        return api;
     };
-
-    var setUpAd = function () {
-      var callback = function () {
-        processSVG(window.bannerSvgData);
-        show();
-        rootSource = document.getElementById("adRoot").cloneNode(true);
-        currentAnimationKill = Animation();
-      };
-      var loader = window.svgImageLoader;
-      loader(window.bannerSvgData, callback);
-    };
-    api.init = function () {
-      if (firstRun) {
-        return
-      }
-      firstRun = true;
-      console.log('ad int');
-      setUpAd();
-    };
-    api.replay = function () {
-      currentAnimationKill();
-      buildRoot();
-      Animation();
-    };
-    return api;
-  };
-  window.ad = Ad();
+    window.ad = Ad();
 })();
